bf54466b1b8ba72972d9d185df801405064364bb
Jerome <jerome-fabricante@isuzuphil.com>
2017-10-24 09:27:07 +0800

    Exam modal content

---
+++ application/views/course/exam_view.php
@@ -0,0 +1,589 @@
+<div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		<span aria-hidden="true">×</span>
+	</button>
+	<h4 class="modal-title"><?php echo $title; ?></h4>
+</div>
+<div class="modal-body">
+	<ol>
+		<li>A type of maintenance wherein tasks are completed when the machines are shut down.</li>
+		<div class = "radio">
+			<label>
+				<input type="radio" name="q1" id="optionsRadios1" value="option1">
+				a. Periodic Maintenance
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q1" id="optionsRadios1" value="option1">
+				b. Predictive Maintenance
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q1" id="optionsRadios1" value="option1">
+				c. Corrective Maintenance
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q1" id="optionsRadios1" value="option1">
+				d. None of the above
+			</label>
+		</div>
+
+		<li>Tightening torque of Oil Drain Plug for Isuzu D-Max.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q2" id="optionsRadios1" value="option1">
+				a. 380 N-m
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q2" id="optionsRadios1" value="option1">
+				b. 44 N-m
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q2" id="optionsRadios1" value="option1">
+				c. 5 N-m
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q2" id="optionsRadios1" value="option1">
+				e. 10 N-m
+			</label>
+		</div>
+
+		<li>It is one of the functions of engine oil which cleans foreign materials such as carbon and metal particles due to wear of metal surfaces.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q3" id="optionsRadios1" value="option1">
+				a. Cleaning Action
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q3" id="optionsRadios1" value="option1">
+				b. Cooling Action
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q3" id="optionsRadios1" value="option1">
+				c. Lubricating Action
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q3" id="optionsRadios1" value="option1">
+				d. None of the above
+			</label>
+		</div>
+
+		<li>It is an organization that provides ratings of oil viscosity</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q4" id="optionsRadios1" value="option1">
+				a. SAE
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q4" id="optionsRadios1" value="option1">
+				b. API
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q4" id="optionsRadios1" value="option1">
+				c. ILSAC
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q4" id="optionsRadios1" value="option1">
+				d. ASTM
+			</label>
+		</div>
+
+		<li>It is the feeler gauge thickness used for Valve Clearance Adjustment of Isuzu D-Max</li>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q5" id="optionsRadios1" value="option1">
+				a. 0.80 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q5" id="optionsRadios1" value="option1">
+				b. 1.00 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q5" id="optionsRadios1" value="option1">
+				c. 0.15 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q5" id="optionsRadios1" value="option1">
+				d. None of the above
+			</label>
+		</div>
+
+		<li>Tightening Torque used after Valve Clearance Adjustment of 6 cylinder engines.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q6" id="optionsRadios1" value="option1">
+				a. 18 N-m
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q6" id="optionsRadios1" value="option1">
+				b. 22 N-m
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q6" id="optionsRadios1" value="option1">
+				c. 5 N-m
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q6" id="optionsRadios1" value="option1">
+				c. Any of the above
+			</label>
+		</div>
+
+		<li>It delivers crankshaft power to the water pump, cooling fan, alternator, power steering, air-con compressor and vacuum pump.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q7" id="optionsRadios1" value="option1">
+				a. Drive Belts
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q7" id="optionsRadios1" value="option1">
+				b. V-belts
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q7" id="optionsRadios1" value="option1">
+				c. Conveyor
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q7" id="optionsRadios1" value="option1">
+				d. None of the above
+			</label>
+		</div>
+
+		<li>It is the standard length of clutch peda free-play</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q8" id="optionsRadios1" value="option1">
+				a. 1.0 m
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q8" id="optionsRadios1" value="option1">
+				b. 6.0 mm to 16.00 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q8" id="optionsRadios1" value="option1">
+				c. 1.0 to 3.0 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q8" id="optionsRadios1" value="option1">
+				d. 5.0 to 15.00 mm
+			</label>
+		</div>
+
+		<li>If the ATF smells and looks burnt out, what should technicians do?</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q9" id="optionsRadios1" value="option1">
+				a. Replace ATF
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q9" id="optionsRadios1" value="option1">
+				b. Clean ATF and return it to the transmission.
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q9" id="optionsRadios1" value="option1">
+				c. Add clean ATF.
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q9" id="optionsRadios1" value="option1">
+				d. Replace Transmission.
+			</label>
+		</div>
+
+		<li>For Isuzu Dmax and mu-X, how much ATF should be added during inspection?</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q10" id="optionsRadios1" value="option1">
+				a. 1000 ml
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q10" id="optionsRadios1" value="option1">
+				b. 300 ml
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q10" id="optionsRadios1" value="option1">
+				c. 500 ml
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q10" id="optionsRadios1" value="option1">
+				d. 10 ml
+			</label>
+		</div>
+
+		<li>It prevents seizure of the gears of the differential.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q11" id="optionsRadios1" value="option1">
+				a. Engine Oil
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q11" id="optionsRadios1" value="option1">
+				b. Differential Gear Oil
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q11" id="optionsRadios1" value="option1">
+				c. Transmission Oil
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q11" id="optionsRadios1" value="option1">
+				d. None of the above
+			</label>
+		</div>
+
+		<li>It is a type of measuring tool used to measure distance between 2 opposite side of an object and gives direct reading of distance with high accuracy.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q12" id="optionsRadios1" value="option1">
+				a. Depth Gauge
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q12" id="optionsRadios1" value="option1">
+				b. Cylinder Bore Gauge
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q12" id="optionsRadios1" value="option1">
+				c. dial Indicator Gauge
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q12" id="optionsRadios1" value="option1">
+				d. Vernier Caliper
+			</label>
+		</div>
+
+		<li>It is a measuriing instrument used to accurately measure small linear disntances.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q13" id="optionsRadios1" value="option1">
+				a. Depth Gauge
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q13" id="optionsRadios1" value="option1">
+				b. Cylinder Bore Gauge
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q13" id="optionsRadios1" value="option1">
+				c. dial Indicator Gauge
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q13" id="optionsRadios1" value="option1">
+				d. Vernier Caliper
+			</label>
+		</div>
+
+		<li>It measures clearances between two parts consisting of small lengths of flexible stell with different thickness.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q14" id="optionsRadios1" value="option1">
+				a. Feeler Gauge
+			</label>
+		</div>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q14" id="optionsRadios1" value="option1">
+				b. Straight Edge
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q14" id="optionsRadios1" value="option1">
+				c. Ruler
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q14" id="optionsRadios1" value="option1">
+				d. Vernier Caliper
+			</label>
+		</div>
+
+		<li>Amount of Engine Coolant used in Isuzu Dmax</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q15" id="optionsRadios1" value="option1">
+				a. 10.0 L
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q15" id="optionsRadios1" value="option1">
+				b. 29.0 L
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q15" id="optionsRadios1" value="option1">
+				c. 8.0 L
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q15" id="optionsRadios1" value="option1">
+				d. 5.0 L
+			</label>
+		</div>
+
+		<li>Transmission Oil used in Isuzu D-max and mu-X TB50LS Automatic Transmission.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q16" id="optionsRadios1" value="option1">
+				a. Havoline multi-Vehicle ATF
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q16" id="optionsRadios1" value="option1">
+				B. Mobil AT3309
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q16" id="optionsRadios1" value="option1">
+				c. Besco Genuine SAE 10W30
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q16" id="optionsRadios1" value="option1">
+				d. Shell Spirax S5 ATF X
+			</label>
+		</div>
+
+		<li>Type of Brake Fluid used on Isuzu Vehicles with hydraulic brakes.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q17" id="optionsRadios1" value="option1">
+				a. DOT 1
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q17" id="optionsRadios1" value="option1">
+				b. DOT 2
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q17" id="optionsRadios1" value="option1">
+				c. DOT 3
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q17" id="optionsRadios1" value="option1">
+				d. DOT 4
+			</label>
+		</div>
+
+		<li>Maintenance Interval for Brake Fluid</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q18" id="optionsRadios1" value="option1">
+				a. Every 5K
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q18" id="optionsRadios1" value="option1">
+				b. Every 10K
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q18" id="optionsRadios1" value="option1">
+				c. Every 20K
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q18" id="optionsRadios1" value="option1">
+				d. As needed
+			</label>
+		</div>
+
+		<li>It is the limit for use of Brake Pads</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q19" id="optionsRadios1" value="option1">
+				a. 5.0 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q19" id="optionsRadios1" value="option1">
+				b. 3.0 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q19" id="optionsRadios1" value="option1">
+				c. 10 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q19" id="optionsRadios1" value="option1">
+				d. 1.5 mm
+			</label>
+		</div>
+
+		<li>It is the standard Brake Pedal Free Play.</li>
+		<div class="radio">
+			<label>
+				<input type="radio" name="q20" id="optionsRadios1" value="option1">
+				a. 6 to 10 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q20" id="optionsRadios1" value="option1">
+				b. 7 to 11 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q20" id="optionsRadios1" value="option1">
+				c. 8 to 12 mm
+			</label>
+		</div>
+
+		<div class="radio">
+			<label>
+				<input type="radio" name="q20" id="optionsRadios1" value="option1">
+				d. Up to 20 mm
+			</label>
+		</div>
+	</ol>
+</div>
